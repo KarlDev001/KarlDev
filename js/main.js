3ef34c60270c7b4f4bad7c4227b143cfdea597f9
KarlDev <carldev.official@gmail.com>
2022-02-20 18:52:57 +0200

    Front End Done.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ toggleMenu.addEventListener('click', () => {
 classMenu.addEventListener('click', () => {
     navMenu.classList.remove('show')
 })
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "carldev.official@gmail.com",
+        Password: "password",
+        To: 'karolis.vas9@gmail.com',
+        From: document.getElementById("email").value,
+        Subject: "New Contact Form Submition From KarlDev.com",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
